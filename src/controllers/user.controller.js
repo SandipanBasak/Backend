831daf55620d1f58977b1438529e215b9e2869aa
SandipanBasak <22051786@kiit.ac.in>
2024-08-04 13:19:53 +0530

    controller for login and logout and fixing some bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,24 @@ import { uploadcloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { stringify } from "flatted";
 
+
+const generateAccessAndRefreshTokens= async (userid)=>{
+    try{
+        const user = await User.findById(userid)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken=refreshToken
+        await user.save({validateBeforeSave: false})
+
+        return {accessToken,refreshToken}
+
+    }catch(error){
+        throw new ApiError(500,"Something went wrong while generating refresh and access tokens")
+
+    }
+}
+
 const registerUser=asyncHandler( async (req,res)=>{
     const {username,fullName,email,password}=req.body;
 
@@ -58,11 +76,81 @@ const registerUser=asyncHandler( async (req,res)=>{
     ))
 })
 
+const loginUser=asyncHandler( async (req,res) => {
+    const {username,email,password}=req.body
 
+    if(!username || !email)
+        throw new ApiError(400,"username or email is required")
 
+    const user= await User.findOne({
+        $or:[{username},{email}]
+    })
+
+    if(!user)
+        throw new ApiError(404,"user does not exist")
+
+    const isPasswordValid = await user.comparePassword(password)
+
+    if(!isPasswordValid)
+        throw new ApiError(401,"Invalid user credentials")
+
+    const {accessToken,refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const options={
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.
+    status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(
+        new ApiResponse(
+            200,
+            {user: loggedInUser, accessToken,refreshToken},
+            "User logged in successfully"
+        )
+    )
+    
+})
+
+const logoutUser= asyncHandler( async(req,res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set:{
+                refreshToken: undefined
+            }
+        },
+        {
+            new:true
+        }
+    )
+    const options={
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken",options)
+    .json(
+        new ApiResponse(
+            200,
+            {},
+            "User logged out successfully"
+        )
+    )
+})
 
 export {
-    registerUser
+    registerUser, 
+    loginUser,
+    logoutUser
 }
 
 
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -59,24 +59,27 @@ userSchema.methods.comparePassword =async function(password){
     return await bcrypt.compare(password,this.password)
 }
 
-userSchema.methods.generateAccessToken= function(){
-    jwt.sign({
-        _id: this._id,
-        email: this.email,
-        username: this.username,
-        fullName: this.fullName
-    },
-    process.env.ACCESS_TOKEN_SECRET,
-    {
-        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
-    }
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
     )
 }
 
-userSchema.methods.generateRefreshToken= function(){
-    jwt.sign({
-        _id: this._id
-    },
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
         process.env.REFRESH_TOKEN_SECRET,
         {
             expiresIn: process.env.REFRESH_TOKEN_EXPIRY
